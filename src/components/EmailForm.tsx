a8d08df49a51767990a1a8a73e4ddcc4735aa493
Kuzyk Bogdan <bogdankuzyk14@gmail.com>
2025-07-13 01:07:16 +0300

    Add email and recipients list fields

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,4 +1,4 @@
-import { Form, Input } from "antd";
+import { Form, Input, AutoComplete, Space } from "antd";
 
 function EmailForm() {
   return (
@@ -9,6 +9,20 @@ function EmailForm() {
       variant={"filled"}
       colon={false}
     >
+      {/* Email */}
+      <Form.Item name={"email"} label={"Email"}>
+        <AutoComplete placeholder="Enter email" />
+      </Form.Item>
+
+      <Form.Item label="Recipients List">
+        <Space
+          className="border border-gray-300 rounded-md h-[130px] p-2 w-full  overflow-y-auto flex-wrap"
+          align="start"
+        >
+          placeholder
+        </Space>
+      </Form.Item>
+
       {/* Subject */}
       <Form.Item
         name="subject"
