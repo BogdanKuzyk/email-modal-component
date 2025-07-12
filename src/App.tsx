0461e47740b1f7a440c1567836577ddafd4ede57
Kuzyk Bogdan <bogdankuzyk14@gmail.com>
2025-07-12 19:54:45 +0300

    Connect modal  to button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import "./App.css";
+import { useState } from "react";
 import { Button } from "antd";
+import "./App.css";
+import UiModal from "./components/UiModal";
 
 function App() {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+  //Events
+  const onEmailModalOpen = (): void => {
+    setModalOpen(true);
+  };
+
+  const onEmailModalClose = (): void => {
+    setModalOpen(false);
+  };
+
   return (
     <>
-      <Button type="primary">Open Email Modal</Button>
+      <Button onClick={onEmailModalOpen} type="primary">
+        Open Email Modal
+      </Button>
+      <UiModal
+        open={modalOpen}
+        title={"Send Email Form"}
+        onCancel={onEmailModalClose}
+      ></UiModal>
     </>
   );
 }
